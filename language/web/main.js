913216858541c69000c640d21220f35c26186753
João Moreira de Sá Coutinho <joao.moreiradsc@gmail.com>
2023-01-12 18:54:45 +0100

    Green checkmark, red cross, attempt validation on change event

--- language/web/main.js
+++ language/web/main.js
@@ -1,3 +1,20 @@
+Vue.component('attempt', {
+    data: function() {
+	return {
+	    attempt: '',
+	}
+    },
+    props: ['word'],
+    template: '<input v-model.lazy="attempt" v-on:change="changed">',
+    methods: {
+	changed: function() {
+	    console.log(`Attempt="${this.attempt}"` +
+			` ${this.attempt === this.word.he ? 'ok': 'NOK'}`)
+	}
+
+    }
+})
+
 const app = new Vue({
     el: '#app',
     data: {
